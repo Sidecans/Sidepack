d4a88bbc4d9c06a3688e890f5d716d7f1e57147b
Sidecans <110431851+Sidecans@users.noreply.github.com>
2022-09-01 11:53:11 -0700

    Made Base Files

---
+++ Windows/FileWizard/FileWiz.py
@@ -0,0 +1,5 @@
+print("Welcome to the File Wizard")
+print("The File Wizard will check the type of a file extension and give you details about the extension")
+print("\n")
+print("\n")
+extens = input("Enter the extension of the file:\n")
